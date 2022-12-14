67d5e4167316e9fade9ce1a4ecb8a695c329a449
Ilyas <opebiyiilyas@gmail.com>
2022-12-14 04:49:41 -0800

    linking to frontend

--- Project_Structure/InstaCard/urls.py
+++ Project_Structure/InstaCard/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('signout/', views.signout, name='signout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
